f30616bc369eb0bc1a470f64b120355289b8dbbe
Johan Berdat <jojolebarjos@gmail.com>
2025-05-08 17:54:55 +0200

    Start anew from a clean state

--- src/alphazero/__init__.py
+++ src/alphazero/__init__.py
@@ -1,5 +1,1 @@
-from .buffer import Buffer
-from .data import Action, Config, Prediction, State, Turn
-from .predictor import BatchedPredictor, BufferedPredictor, Predictor, RandomPredictor
-from .sampler import sample_action, sample_episode
-from .search import Search, SearchPredictor
+__version__ = "0.2.0"
