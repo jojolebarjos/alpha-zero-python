9d97ed8ad6dc0ae614fcf5dd0cac4fabf1a8e2f3
Johan Berdat <jojolebarjos@gmail.com>
2025-05-08 20:31:33 +0200

    Move episode generator code to dedicated sampler class

--- src/alphazero/worker/__main__.py
+++ src/alphazero/worker/__main__.py
@@ -1,42 +1,11 @@
-import numpy as np
-
-from alphazero.data import Episode
-from alphazero.searcher import Searcher
 from alphazero.worker.remote.dummy import DummyRemote
+from alphazero.worker.sampler import Sampler
 
 
 # TODO load this from CLI argument, somehow
 
 remote = DummyRemote()
 
-config = remote.config
-batch_size = 8
-
-
-# TODO move this code into proper helper functions/classes
-
-episodes = [Episode([config.sample_initial_state()], [], []) for _ in range(batch_size)]
-
+sampler = Sampler(remote, batch_size=8)
 while True:
-    predictor = remote.predictor
-    # TODO should probably reuse part of the subtree
-    predictor = Searcher(predictor, num_steps=400, c_puct=4.0)
-
-    states = [episode.states[-1] for episode in episodes]
-    predictions = predictor.predict_many(states)
-
-    for i in range(batch_size):
-        prediction = predictions[i]
-        # TODO temperature?
-        action = np.random.choice(prediction.actions, p=prediction.policy)
-
-        state = action.sample_next_state()
-
-        episode = episodes[i]
-        episode.predictions.append(prediction)
-        episode.actions.append(action)
-        episode.states.append(state)
-
-        if state.has_ended:
-            remote.add_episode(episode)
-            episodes[i] = Episode([config.sample_initial_state()], [], [])
+    sampler.step()
---
+++ src/alphazero/worker/sampler.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+from alphazero.data import Episode
+from alphazero.searcher import Searcher
+from alphazero.worker.remote import Remote
+
+
+class Sampler:
+    """..."""
+
+    def __init__(self, remote: Remote, batch_size: int) -> None:
+        self.remote = remote
+        self.batch_size = batch_size
+        self.episodes = [Episode([self.remote.config.sample_initial_state()], [], []) for _ in range(self.batch_size)]
+
+    def step(self) -> None:
+        """..."""
+
+        predictor = self.remote.predictor
+        # TODO should probably reuse part of the subtree
+        predictor = Searcher(predictor, num_steps=400, c_puct=4.0)
+
+        states = [episode.states[-1] for episode in self.episodes]
+        predictions = predictor.predict_many(states)
+
+        for i in range(self.batch_size):
+            prediction = predictions[i]
+            # TODO temperature?
+            action = np.random.choice(prediction.actions, p=prediction.policy)
+
+            state = action.sample_next_state()
+
+            episode = self.episodes[i]
+            episode.predictions.append(prediction)
+            episode.actions.append(action)
+            episode.states.append(state)
+
+            if state.has_ended:
+                self.remote.add_episode(episode)
+                self.episodes[i] = Episode([self.remote.config.sample_initial_state()], [], [])
