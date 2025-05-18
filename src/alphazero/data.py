c7c55f6657ec6f240b9c26fb47d0196944edc357
Johan Berdat <jojolebarjos@gmail.com>
2025-05-18 14:54:47 +0200

    Add data model serialization

--- src/alphazero/data.py
+++ src/alphazero/data.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Any, TypeAlias
+from typing import Any, Self, TypeAlias
 
 import numpy as np
 
@@ -23,6 +23,22 @@ class Prediction:
     policy: np.ndarray
     value: np.ndarray
 
+    def to_json(self) -> Any:
+        return {
+            "actions": [action.to_json() for action in self.actions],
+            "policy": self.policy.tolist(),
+            "value": self.value.tolist(),
+        }
+
+    @classmethod
+    def from_json(cls, payload: Any, state: State) -> Self:
+        actions = [state.Action.from_json(action_payload, state) for action_payload in payload["actions"]]
+        policy = np.array(payload["policy"], dtype=np.float32)
+        assert policy.shape == (len(actions),)
+        value = np.array(payload["value"], dtype=np.float32)
+        assert value.shape == (state.config.num_players,)
+        return cls(actions, policy, value)
+
 
 @dataclass
 class Episode:
@@ -36,6 +52,29 @@ class Episode:
     states: list[State]
     predictions: list[Prediction]
     actions: list[int]
+
+    def to_json(self) -> Any:
+        return {
+            "states": [state.to_json() for state in self.states],
+            "predictions": [prediction.to_json() for prediction in self.predictions],
+            "actions": self.actions,
+        }
+
+    @classmethod
+    def from_json(cls, payload: Any, config: Config) -> Self:
+        states = [config.State.from_json(state_payload, config) for state_payload in payload["states"]]
+        assert len(states) > 0
+        assert len(payload["predictions"]) == len(states) - 1
+        predictions = [
+            Prediction.from_json(prediction_payload, state)
+            for prediction_payload, state in zip(payload["predictions"], states)
+        ]
+        assert len(predictions) == len(states) - 1
+        actions = payload["actions"]
+        assert len(actions) == len(states) - 1
+        for prediction, action in zip(predictions, actions):
+            assert 0 <= action < len(prediction.actions)
+        return cls(states, predictions, actions)
 
 
 @dataclass
@@ -54,3 +93,19 @@ class Sample:
     state: State
     policy: np.ndarray
     value: np.ndarray
+
+    def to_json(self) -> Any:
+        return {
+            "state": self.state.to_json(),
+            "policy": self.policy.tolist(),
+            "value": self.value.tolist(),
+        }
+
+    @classmethod
+    def from_json(cls, payload: Any, config: Config) -> Self:
+        state = config.State.from_json(payload["state"], config)
+        policy = np.array(payload["policy"], dtype=np.float32)
+        assert policy.shape == (len(state.actions),)
+        value = np.array(payload["value"], dtype=np.float32)
+        assert value.shape == (config.num_players,)
+        return cls(state, policy, value)
